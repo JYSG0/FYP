aed2ca40d6952fbd528cbb7c5c82e8eb134807f9
JYSG0 <147217854+JYSG0@users.noreply.github.com>
2024-11-13 12:52:03 +0800

    g
    g

---
+++ YOLOP/run.py
@@ -0,0 +1,23 @@
+from tools.demo import YOLOP
+import argparse
+import torch
+from lib.config import cfg, update_config
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='weights/End-to-end.pth', help='model.pth path(s)')
+    parser.add_argument('--source', type=str, default='6', help='source')  # file/folder   ex:inference/images
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='cpu', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--save-dir', type=str, default='inference/output', help='directory to save results')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    opt = parser.parse_args()
+    with torch.no_grad():
+        # Create an instance of YOLOP class
+        yolop_instance = YOLOP(cfg, opt)
+
+        # Run detection using the instance
+        yolop_instance.detect()
