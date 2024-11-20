feb21452270762c04207267a995de8387b712df2
Amanda <amandayap.my@gmail.com>
2024-11-20 14:12:08 +0800

    Running both YOLO AIs (with depth cam) :D

---
+++ YOLOP/runboth.py
@@ -0,0 +1,35 @@
+from tools.demo import YOLOP
+import argparse
+import torch
+from lib.config import cfg, update_config
+import cv2
+
+def main():
+    global exit_flag
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='weights/End-to-end.pth', help='model.pth path(s)')
+    parser.add_argument('--source', type=str, default='6', help='source')  # file/folder   ex:inference/images
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='0,1,2,3', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--save-dir', type=str, default='inference/output', help='directory to save results')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    opt = parser.parse_args()
+
+    while not exit_flag:
+        with torch.no_grad():
+
+            # Create an instance of YOLOP class
+            yolop_instance = YOLOP(cfg, opt)
+            yolop_instance.detect()
+
+            cv2.waitKey(1) 
+
+exit_flag = False
+
+if __name__ == '__main__':
+
+    main()
