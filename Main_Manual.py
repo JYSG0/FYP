10f1dd6d41b21901757f609f0d3b1c64256b08b9
JYSG0 <147217854+JYSG0@users.noreply.github.com>
2024-11-26 10:30:12 +0800

    Update Main_Manual.py

--- Main_Manual.py
+++ Main_Manual.py
@@ -5,11 +5,40 @@ import pygame
 import time
 import os
 from realsense_depth import *
+import Jetson.GPIO as GPIO
+import busio
+import board
+import digitalio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# list_board_pins()
+i2c=busio.I2C(board.SCL_1, board.SDA_1)
+time.sleep(0.1)  # Small delay to stabilize the connection
+ads=ADS.ADS1115(i2c, address=0x48)
+
+chan1 = AnalogIn(ads, ADS.P0)
+chan2=AnalogIn(ads, ADS.P3)
+print(chan1.value, chan1.voltage)
+print(chan2.value, chan2.voltage)
+
+
+# Function to map chan1 value to a specific range (e.g., 10 to 100)
+def map_potentiometer_value(value, input_min=0, input_max=26230, output_min=0, output_max=1023):
+    return ((value - input_min) * (output_max - output_min)) / (input_max - input_min) + output_min
+
+#Initialise GPIO Pins
+pwm = digitalio.DigitalInOut(board.D12)
+steering = digitalio.DigitalInOut(board.D13)
+pwm.direction = digitalio.Direction.OUTPUT
+steering.direction = digitalio.Direction.OUTPUT
+
+#Initialise actuator IDLE 
+pwm.value = True  # Set GPIO12 high
+steering.value = True  # Set GPIO13 high
+
 # Global variable to track drivable area detection and object detection
 Steering = False
-#object detection class_id
-class_ids=[]
-
 
 # global cap for camera
 cap = DepthCamera()
@@ -119,6 +148,12 @@ odrv0.axis1.requested_state = 8 # Start Motor
 
 def Manual_drive(keys):
         global Steering
+         # Read potentiometer value
+        try:
+            pot_value = map_potentiometer_value(chan1.value)
+        except OSError as e:
+            print(f"Error reading potentiometer: {e}")
+
         # 'e' key to start motor
         if keys[pygame.K_e]:  
             print("Starting...")
@@ -127,13 +162,13 @@ def Manual_drive(keys):
 
         # 'w' key to move forward
         if keys[pygame.K_w]:
-            odrv0.axis1.controller.input_vel = 2
+            odrv0.axis1.controller.input_vel = -2
             odrv0.axis0.controller.input_vel = -2   
             print ('FORWARD')
 
         # 's' key to move backwards
         if keys[pygame.K_s]:
-                odrv0.axis1.controller.input_vel = -2
+                odrv0.axis1.controller.input_vel = 2
                 odrv0.axis0.controller.input_vel = 2                
                 print ('BACKWARD')
 
@@ -147,24 +182,31 @@ def Manual_drive(keys):
             print ('STOP')
 
         #'a' is pressed
-        if keys[pygame.K_a]:  # Steer Left
-            # GPIO.output(pwm, GPIO.LOW)
-            # GPIO.output(steering, GPIO.HIGH)
-            print ('LEFT')
+        if keys[pygame.K_a] and pot_value <= 800:  # Steer Left
+            pwm.value = False
+            steering.value = True
+            if pot_value is not None:
+                print(f"Steering Left: Potentiometer Value: {pot_value}")
+                print ('LEFT')
             Steering = True
 
         #'d' is pressed
-        if keys[pygame.K_d]:  # Steer Right
-            # GPIO.output(pwm, GPIO.LOW)
-            # GPIO.output(steering, GPIO.LOW) 
-            print('RIGHT')
+        if keys[pygame.K_d] and pot_value >= 300:  # Steer Right
+            pwm.value = False
+            steering.value = False
+            if pot_value is not None:
+                print(f"Steering Right: Potentiometer Value: {pot_value}")
+                print('RIGHT')
             Steering = True
 
         if not keys[pygame.K_a] and not keys[pygame.K_d]:  # Joystick IDLE
-            # GPIO.output(pwm, GPIO.HIGH)
-            # GPIO.output(steering, GPIO.HIGH)
+            pwm.value = True
+            steering.value = True
+            if pot_value is not None:
+                print(f"No Steering: Potentiometer Value: {pot_value}")
             Steering = False
 
+        return pot_value
         
 def main():
     # global exit variable
@@ -194,7 +236,10 @@ def main():
                 odrv0.axis1.requested_state = 3  # Set ODrive to idle state
                 odrv0.axis0.requested_state = 3  # Set ODrive to idle state
 
-
+        pot_value = Manual_drive(keys)
+        if pot_value == None:
+            continue
+        
         # If recording, write the frame to the video output file
         if recording:
             out_annotated.write(annotated_frame)
